cc6c949e8a8173e6281b2e0c17a0acea62ebf8ac
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2022-02-21 22:39:56 +0300

    Добавление функции video

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,7 @@ import "./components/progress"
 import "./components/accordions"
 import "./components/portfolio-tabs"
 import "./components/selects"
+import "./components/video"
 
 // CSS
 import "../styles/main.scss"
